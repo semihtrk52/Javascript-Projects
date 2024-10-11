f6f36fe259491c6ca377558cb57c0a1f0a245609
semihtrk52 <semihturku5252@gmail.com>
2024-10-12 01:05:30 +0300

    child to parent

--- To-Do-App/src/App.jsx
+++ To-Do-App/src/App.jsx
@@ -4,10 +4,18 @@ import TodoCreate from './components/TodoCreate';
 import TodoList from './components/TodoList';
 
 function App() {
+  const [todos, setTodos] = useState([]);
+
+  const createTodo = (newTodo) => {
+    setTodos([...todos, newTodo]);
+  };
+
+  console.log(todos);
+
   return (
     <div className="App">
       <div className="app-border">
-        <TodoCreate />
+        <TodoCreate onCreateTodo={createTodo} />
         <TodoList />
       </div>
     </div>
--- To-Do-App/src/components/TodoCreate.jsx
+++ To-Do-App/src/components/TodoCreate.jsx
@@ -1,11 +1,30 @@
-import React from 'react';
+import React, { useState } from 'react';
 import '../App.css';
 
-function TodoCreate() {
+function TodoCreate({ onCreateTodo }) {
+  const [newTodo, setNewTodo] = useState('');
+
+  const createTodo = () => {
+    if (!newTodo) return;
+    const request = {
+      id: Math.floor(Math.random() * 99999),
+      content: newTodo,
+    };
+    onCreateTodo(request);
+    setNewTodo('');
+  };
+
   return (
     <div className="todo-create">
-      <input type="text" placeholder="Todo giriniz" />
-      <button>Todo Oluştur</button>
+      <input
+        value={newTodo}
+        onChange={(e) => {
+          setNewTodo(e.target.value);
+        }}
+        type="text"
+        placeholder="Todo giriniz"
+      />
+      <button onClick={createTodo}>Todo Oluştur</button>
     </div>
   );
 }
